0e9f2d515cc5b05d79c3d4844312b64a56b02b04
Nirav Ranpara <geniousnirav@gmail.com>
2013-08-16 17:16:31 +0530

    image differ

---
+++ imagediff/src/com/imagediff/ImagediffActivity.java
@@ -0,0 +1,13 @@
+package com.imagediff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImagediffActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
